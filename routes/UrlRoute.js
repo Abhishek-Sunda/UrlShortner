9e2138873225429b9796bbb147928de874ccc6c4
Dev-Abhishek-Sunda <149408279+Abhishek-Sunda@users.noreply.github.com>
2025-07-15 04:32:51 +0000

    Testing CodeSpace

--- routes/UrlRoute.js
+++
@@ -1,7 +0,0 @@
-const Express  = require('express')
-const router   = Express.Router();
-const {GnrtNewShortUrl} = require('../Controllers/UrlController');
-
-router.post("/",GnrtNewShortUrl)
-
-module.exports = router;
